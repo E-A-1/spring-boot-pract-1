1bf9db11aaa8c669151873249c93b90d68cd812c
EAVijay <elijaarulin.vijayakumar@troniqsrationale.com>
2023-07-21 00:35:10 +0530

    sdfasdf

---
+++ src/main/java/com/example/demosfdsdfsadffdsfd/DemosfdsdfsadffdsfdApplication.java
@@ -0,0 +1,13 @@
+package com.example.demosfdsdfsadffdsfd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemosfdsdfsadffdsfdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemosfdsdfsadffdsfdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demosfdsdfsadffdsfd/DemosfdsdfsadffdsfdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demosfdsdfsadffdsfd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemosfdsdfsadffdsfdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
